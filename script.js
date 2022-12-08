8fbcadae72e5c82aa3010695f354c703a359b30b
Mohan Krishna <113287292+GMKrishna99@users.noreply.github.com>
2022-12-08 15:56:53 +0530

    changes-3

--- script.js
+++ script.js
@@ -18,4 +18,61 @@ rightbtn.addEventListener('click',
     event,preventDefault();
 });
 
+const leftbtnl = document.querySelector(".btn-l");
+const rightbtnr = document.querySelector(".btn-r");
+
+leftbtnl.addEventListener('click' , function (event){
+    console.log('done');
+    const conent = document.querySelector('.ps-1');
+    conent.scrollLeft -= 1100;
+    event,preventDefault();
+
+})
+
+rightbtnr.addEventListener('click', 
+    function (event){
+    console.log('done');
+    const conent = document.querySelector('.ps-1');
+    conent.scrollLeft += 1100;
+    event,preventDefault();
+});
+
+const leftbtnla = document.querySelector(".btn-la");
+const rightbtnra = document.querySelector(".btn-ra");
+
+leftbtnla.addEventListener('click' , function (event){
+    console.log('done');
+    const conent = document.querySelector('.ps-1a');
+    conent.scrollLeft -= 1100;
+    event,preventDefault();
+
+})
+
+rightbtnra.addEventListener('click', 
+    function (event){
+    console.log('done');
+    const conent = document.querySelector('.ps-1a');
+    conent.scrollLeft += 1100;
+    event,preventDefault();
+});
+
+const leftbtnlab = document.querySelector(".btn-lab");
+const rightbtnrab = document.querySelector(".btn-rab");
+
+leftbtnlab.addEventListener('click' , function (event){
+    console.log('done');
+    const conent = document.querySelector('.ps-1ab');
+    conent.scrollLeft -= 1100;
+    event,preventDefault();
+
+})
+
+rightbtnrab.addEventListener('click', 
+    function (event){
+    console.log('done');
+    const conent = document.querySelector('.ps-1ab');
+    conent.scrollLeft += 1100;
+    event,preventDefault();
+});
+
 // slide buttons function end
